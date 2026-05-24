84919376ab02af9816b03e5d82bb226bded8cdc2
Rachita Laad <rachitalaad505@gmail.com>
2026-05-24 17:49:51 +0530

    Footer and CTA done

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,8 +1,64 @@
 import React from 'react';
+import { FaLinkedin , FaTwitterSquare , FaGithubSquare} from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa";
 
 function Footer() {
     return (
-        <h1>Footer</h1>
+        <footer className='pt-5 pb-3 mt-5 border-top' style={{backgroundColor : "#f8f8f7"}}>
+            <div className="container">
+                <div className="row align-items-start justify-content-between">
+                    <div className='col-md-4 mb-4'>
+                        <h3 className='fw-bold' style={{color: "#002855"}}>InvestIQ</h3>
+                        <p className='text-muted mt-3 fw-semibold' style={{lineHeight: "1.8" ,  maxWidth: "420px"}}>
+                             Smart investment platform for portfolio tracking,
+              analytics, and real-time market insights.
+                        </p>
+                    </div>
+
+                    <div className='col-md-4 mb-4'>
+                        <h5 className='mb-4 fw-bold'>Quick Links</h5>
+                          <div className='d-flex gap-4 flex-wrap text-muted fw-semibold'>
+
+                            <span style={{ cursor: "pointer" }}>
+                                Features
+                            </span>
+
+                            <span style={{ cursor: "pointer" }}>
+                                Insights
+                            </span>
+
+                            <span style={{ cursor: "pointer" }}>
+                                Pricing
+                            </span>
+
+                            <span style={{ cursor: "pointer" }}>
+                                Markets
+                            </span>
+
+                            <span style={{ cursor: "pointer" }}>
+                                Login
+                            </span>
+
+                        </div>
+
+                    </div>
+
+                    <div className='col-md-3 mb-4'>
+                        <h5 className='mb-4 fw-bold'>Connect</h5>
+                        <div className='d-flex gap-3 fs-3 mt-1'>
+                            <FaLinkedin className='text-secondary fw-semibold' style={{cursor: "pointer"}}/>
+                            <FaGithubSquare className='text-secondary fw-semibold' style={{cursor: "pointer"}}/>
+                            <FaTwitterSquare  className='text-secondary fw-semibold' style={{cursor: "pointer"}}/>
+                        </div>
+                    </div>
+                </div>
+
+                <hr className='mt-4' />
+                <div className='text-center text-muted fw-semibold'>
+                    <p> <FaRegCopyright className='me-1'/> 2026 InvestIQ. All rights reserved.</p>
+                </div>
+            </div>
+        </footer>
     );
 }
 
--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
 
 function OpenAccount() {
-    return (
-        <h1>open</h1>
-    );
+  return (
+    <div className="container p-5 text-center">
+      <h1 className="mb-4 fw-bold" style={{fontSize: "2rem"}}>Start Your Investment Journey Today</h1>
+       <p
+        className="text-muted mb-4"
+        style={{
+          maxWidth: "700px",
+          margin: "0 auto"
+        }}
+      >
+        Smart investing tools designed for modern investors.
+      </p>
+      <button
+        className="btn btn-primary px-4 py-2"
+        style={{ backgroundColor: "#387ed1" ,border: "none" , fontSize: "1rem" , borderRadius: "8px"}}
+      >
+        Create Free Account
+      </button>
+    </div>
+  );
 }
 
 export default OpenAccount;
