1f90b7e34aba46d325494087e8669c2fb0d7a9a9
Rachita Laad <rachitalaad505@gmail.com>
2026-05-24 16:25:08 +0530

    navbar done

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,46 +1,77 @@
-import React from 'react';
+import React from "react";
+import logoImage from "../assets/logo2.png";
 
 function Navbar() {
-    return (
-        <div className="container">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+  return (
+    <nav
+      class="navbar navbar-expand-lg bg-white border-bottom sticky-top py-3"
+      style={{ backgroundColor: "#ffffff" }}
+    >
+      <div class="container">
+        <a class="navbar-brand d-flex align items-center fw-bold fs-4" href="#">
+          <img
+            src={logoImage}
+            alt="InvestIQ Logo Image"
+            className="img"
+            style={{ width: "48px", marginRight: "10px" }}
+          />
+          <span style={{ color: "#002855" }}>InvestIQ</span>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto align-items-center gap-lg-4">
+            <li class="nav-item">
+              <a class="nav-link fw-semibold" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fw-semibold" href="#">
+                Insights
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fw-semibold" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fw-semibold" href="#">
+                Markets
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fw-semibold" href="#">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <button
+                className="btn btn-primary px-4 py-2 fw-semibold"
+                style={{
+                  backgroundColor: "#387ed1",
+                  border: "none",
+                }}
+              >
+                Get Started
+              </button>
+            </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
         </div>
-    );
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
